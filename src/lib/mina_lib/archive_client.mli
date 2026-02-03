81e00bc507f022c76d9fca0894943faed6320112
Christian Despres <christian@o1labs.org>
2026-02-03 14:24:43 -0500

    f - expose events

--- src/lib/mina_lib/archive_client.mli
+++ src/lib/mina_lib/archive_client.mli
@@ -1,6 +1,18 @@
 open Core
 open Pipe_lib
 
+type Structured_log_events.t +=
+  | Archive_block_dispatched of
+      { state_hash : Mina_base.State_hash.t; time : float }
+
+type Structured_log_events.t +=
+  | Archive_dispatch_failed of
+      { state_hash : Mina_base.State_hash.t; error : Yojson.Safe.t }
+
+val archive_block_dispatched_structured_events_id : Structured_log_events.id
+
+val archive_dispatch_failed_structured_events_id : Structured_log_events.id
+
 val dispatch_precomputed_block :
      ?max_tries:int
   -> Host_and_port.t Cli_lib.Flag.Types.with_name
